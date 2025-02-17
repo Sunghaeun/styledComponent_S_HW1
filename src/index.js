a1d6ac1105743dea2962552ce5958424dba13f85
sungyu0309 <tjsrbyt@gmail.com>
2025-02-17 16:26:42 +0900

    style : globalStyle & globalTheme 정의

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { CssBaseline } from "@mui/material";
+import GlobalStyle from "./style/globalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/globalTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
