9178f50a20c3d2fdebcfb56170f40394368570f1
sungyu0309 <tjsrbyt@gmail.com>
2025-02-19 19:59:58 +0900

    style : 파비콘 적용

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import theme from "./style/globalTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
+    {/* <React.StrictMode> */}
     <CssBaseline />
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+    {/* </React.StrictMode> */}
+  </>
 );
